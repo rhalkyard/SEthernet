rhalkyard/SEthernet

commit eec45d1e9658bfd78ce5c77dff64bb0697503e72

Refactor a bunch of stuff
- Make a bunch of function arguments const

- Tidy up comments

- Replace ad-hoc receive header processing with struct

software/driver/protocolhandler.c
@@ -14,13 +14,15 @@ extern void * header_start;
 
 /* Find a protocol-handler table entry matching protocol number 'theProtocol'.
 Returns a pointer to the entry. */
-protocolHandlerEntry* findPH(driverGlobalsPtr theGlobals, unsigned short theProtocol) {
-    for (unsigned short i = 0; i < numberOfPhs; i++) {
-        if (theGlobals->protocolHandlers[i].ethertype == theProtocol) {
-            return &theGlobals->protocolHandlers[i];
-        }
+protocolHandlerEntry *findPH(const driverGlobalsPtr theGlobals,
+                             const unsigned short theProtocol) {
+  for (unsigned short i = 0; i < numberOfPhs; i++) {
+    if (theGlobals->protocolHandlers[i].ethertype == theProtocol &&
+        theGlobals->protocolHandlers[i].handler != nil) {
+      return &theGlobals->protocolHandlers[i];
     }
-    return nil;
+  }
+  return nil;
 }
 
 /*
@@ -82,11 +84,6 @@ OSStatus doEAttachPH(driverGlobalsPtr theGlobals, const EParamBlkPtr pb) {
     thePHSlot->handler = (void *)pb->u.EParms1.ePointer;
     // thePHSlot->readPB = -1; /* not used */
 
-    if (theGlobals->phCount == 0) {
-      enc624j600_start(&theGlobals->chip);
-    }
-    theGlobals->phCount++;
-
     return noErr;
   }
 }
@@ -109,16 +106,6 @@ OSStatus doEDetachPH(driverGlobalsPtr theGlobals, const EParamBlkPtr pb) {
     // if (thePHSlot->readPB != -1){
     //     /* Cancel pending ERead calls */
     // }
-    theGlobals->phCount--;
-    if (theGlobals->phCount == 0) {
-      enc624j600_suspend(&theGlobals->chip);
-    }
-
-#if defined(DEBUG)
-    strbuf[0] = sprintf(strbuf+1, "Uninstalling handler for protocol %04x", 
-                        pb->u.EParms1.eProtType);
-    DebugStr((unsigned char *)strbuf);
-#endif
 
     return noErr;
   } else

software/driver/driver.c
@@ -48,11 +48,10 @@ the end of the WDS is signaled by an entry with a zero length. The ethernet
 header is already prepared for us, we just have to write our hardware address
 into the source field.
 */
-static OSErr doEWrite(driverGlobalsPtr theGlobals, EParamBlkPtr pb) {
+static OSErr doEWrite(const driverGlobalsPtr theGlobals, const EParamBlkPtr pb) {
   WDSElement *wds; /* a WDS is a list of address-length pairs like an iovec */
-  short entryLen;  /* length of current WDS entry */
   unsigned long totalLength; /* total length of frame */
-  Byte *source, *dest;
+  Byte *dest;
 
   /* Scan through WDS list entries to compute total length */
   wds = (WDSElement *)pb->u.EParms1.ePointer;
@@ -62,9 +61,10 @@ static OSErr doEWrite(driverGlobalsPtr theGlobals, EParamBlkPtr pb) {
     wds++;
   } while (wds->entryLength);
 
-  /* Block transmission of oversized frames. (Add 4 bytes to calcullated length
-  to account for FCS field generated by ethernet controller) */
-  if (totalLength + 4 > 1518) {
+  /* Block transmission of oversized or unreasonably short frames. (Add 4 bytes
+  to calculated length to account for FCS field generated by ethernet
+  controller) */
+  if (totalLength + 4 > 1518 || totalLength < 14) {
   #if defined(DEBUG)
     strbuf[0] = sprintf(strbuf+1, "TX: bogus length %lu bytes!", totalLength);
     DebugStr((unsigned char *)strbuf);
@@ -74,27 +74,22 @@ static OSErr doEWrite(driverGlobalsPtr theGlobals, EParamBlkPtr pb) {
 
   /* Restore WDS pointer to start of list */
   wds = (WDSElement *)pb->u.EParms1.ePointer;
-  source = (Byte *)wds->entryPtr;
   dest = enc624j600_addr_to_ptr(&theGlobals->chip, ENC_TX_BUF_START);
-  entryLen = wds->entryLength;
 
   /* Copy data from WDS into transmit buffer */
   do {
-    enc624j600_memcpy(dest, source, entryLen);
-    dest += entryLen;
+    enc624j600_memcpy(dest, (Byte *)wds->entryPtr, wds->entryLength);
+    dest += wds->entryLength;
     wds++;
-    entryLen = wds->entryLength;
-    source = (Byte *)wds->entryPtr;
-  } while (entryLen > 0);
+  } while (wds->entryLength > 0);
 
   /* Go back and copy our address into the source field */
   dest = enc624j600_addr_to_ptr(&theGlobals->chip, ENC_TX_BUF_START + 6);
-
   enc624j600_memcpy(dest, theGlobals->info.ethernetAddress, 6);
 
   if (theGlobals->chip.link_state == LINK_DOWN) {
     /* don't bother trying to send packets on a down link */
-    return 0; /* TODO: find an appropriate error code */
+    return excessCollsns;
   }
 
   /* Send it! */
@@ -300,14 +295,10 @@ OSErr driverOpen(__attribute__((unused)) EParamBlkPtr pb, AuxDCEPtr dce) {
                                theGlobals->info.ethernetAddress);
       }
 
-      /* Set up read pointers to the start of the receive FIFO */
-      theGlobals->nextPkt =
-          enc624j600_addr_to_ptr(&theGlobals->chip, ENC_RX_BUF_START);
-
 #if defined(TARGET_SE30)
       /* Install our interrupt handler using the Slot Manager */
       theGlobals->theSInt.sqType = sIQType;
-      theGlobals->theSInt.sqPrio = 250;
+      theGlobals->theSInt.sqPrio = 199; /* priority >=200 is reserved for Apple */
       theGlobals->theSInt.sqAddr = driverISR;
       theGlobals->theSInt.sqParm = (long)theGlobals;
       SIntInstall(&theGlobals->theSInt, dce->dCtlSlot);

software/driver/driver.h
@@ -55,25 +55,40 @@ struct multicastEntry {
 };
 typedef struct multicastEntry multicastEntry;
 
+/* Receive Header Area that packet headers (including the ENC624J600's
+next-packet pointer and Receive Status Vector) are read into before calling a
+Protocol Handler. */
+struct receiveHeaderArea {
+  /* ENC624J600 stuff */
+  unsigned short nextPkt_le;  /* pointer to next packet (little-endian, relative 
+                                 to chip address space) */
+  enc624j600_rsv rsv;         /* Receive status vector */
+  /* Actual Ethernet data begins here */
+  Byte dest[6];               /* Destination Ethernet address */
+  Byte source[6];             /* Source Ethernet address */
+  unsigned short protocol;    /* Ethernet Protocol field */
+  /* 8 bytes following the packet header must be provided as workspace for the 
+  Protocol Handler */
+  Byte workspace[8];          /* Protocol handler workspace */
+};
+typedef struct receiveHeaderArea receiveHeaderArea;
+
 /* Global state used by the driver */
 typedef struct driverGlobals {
   enc624j600 chip; /* Ethernet chip state */
 
-  unsigned short phCount;
+  SlotIntQElement theSInt; /* Our slot interrupt queue entry */
+  AuxDCEPtr driverDCE;     /* Our device control entry */
+  Boolean usingVM; /* are we running with Virtual Memory enabled? */
+
   protocolHandlerEntry
       protocolHandlers[numberOfPhs];             /* Protocol handler table */
   multicastEntry multicasts[numberofMulticasts]; /* Multicast address table */
 
-  /* Receive Header Area that packet headers are read into before calling a
-  Protocol Handler. 8 bytes following the packet header must be provided as
-  workspace for the Protocol Handler */
-  Byte recvHeaderArea[8 + 14 + 8];
+  receiveHeaderArea rha;
 
-  SlotIntQElement theSInt; /* Our slot interrupt queue entry */
-  AuxDCEPtr driverDCE;     /* Our device control entry */
-
-  Byte* nextPkt; /* pointer to next packet in receive FIFO */
-
-  Boolean usingVM; /* are we running with Virtual Memory enabled? */
-  driverInfo info; /* Device info packaged up suitably for EGetInfo */
+  /* The driverInfo struct is packed (dictated by the Ethernet driver API).
+  Align its start point to avoid awkwardness in accessing its longword counter
+  fields. */
+  driverInfo info __attribute__((aligned (2)));
 } driverGlobals, *driverGlobalsPtr;

software/driver/isr.c
@@ -37,7 +37,7 @@ On protocol handler entry:
   A1: driver-specific ReadPacket argument (pointer to protocol handler)
   A3: pointer into Receive Header Area, immediately after the header bytes
   A4: pointer to ReadPacket/ReadRest routine
-  D1: number of bytes remaining in packet
+  D1: number of bytes in packet (excluding header and FCS)
 
 The handler calls ReadPacket/ReadRest with the above register definitions, but
 may return with them destroyed.
@@ -50,13 +50,12 @@ C assumes that A0-A1 and D0-D2 will be destroyed, we just need to save A2, A3,
 and D3.
 */
 static void callPH(enc624j600 *chip, void *phProc, Byte *payloadPtr,
-                   void (*readPacketProc)(), unsigned short payloadLen) {
+                   unsigned short payloadLen) {
   asm volatile (
     "   MOVE.L    %[chip], %%a0 \n\t"
     "   MOVE.L    %[phProc], %%a1 \n\t"
     "   MOVE.L    %[payloadPtr], %%a3 \n\t"
     "   MOVE.L    %[readPacketProc], %%a4 \n\t"
-    "   CLR.L     %d1 \n\t" /* Just in case? */
     "   MOVE.W    %[payloadLen], %%d1 \n\t"
     /* Save and restore A5 'manually' as workaround for bug in Retro68's
     modified GCC - see https://github.com/autc04/Retro68/issues/220 */
@@ -67,7 +66,7 @@ static void callPH(enc624j600 *chip, void *phProc, Byte *payloadPtr,
     : [chip] "m" (chip),
       [phProc] "m" (phProc),
       [payloadPtr] "m" (payloadPtr),
-      [readPacketProc] "m" (readPacketProc),
+      [readPacketProc] "m" (ReadPacket),
       [payloadLen] "m" (payloadLen)
     : "a0", "a1", "a2", "a3", "a4", "a5", "d0", "d1", "d2", "d3"
   );
@@ -75,19 +74,10 @@ static void callPH(enc624j600 *chip, void *phProc, Byte *payloadPtr,
 
 /* Handle a packet from the receive FIFO */
 static void handlePacket(driverGlobalsPtr theGlobals) {
-  enc624j600_rsv *rsv;           /* Receive status vector */
-  Byte *destMac;                 /* Destination ethernet address */
-  unsigned short protocol;       /* Protocol number/ethertype */
   unsigned short pktLen;         /* Length of packet */
   unsigned short bytesPending;   /* Number of bytes pending in receive FIFO */
   unsigned short packetsPending; /* Number of packets pending in receive FIFO */
   protocolHandlerEntry *protocolSlot; /* Protocol handler */
-  Byte *rhaPtr = theGlobals->recvHeaderArea; /* Pointer into header buffer */
-  /* Length of packet header (including ENC624J600 data) */
-  const unsigned short headerLen = sizeof(unsigned short) +  /* next-packet pointer */
-                                   sizeof(enc624j600_rsv) +  /* RSV */
-                                   6 * 2 +                   /* dest + src addrs */
-                                   sizeof(unsigned short);   /* ethertype/size */
 
   /* Record some FIFO stats */
   packetsPending = enc624j600_read_rx_pending_count(&theGlobals->chip);
@@ -99,44 +89,16 @@ static void handlePacket(driverGlobalsPtr theGlobals) {
     theGlobals->info.rxPendingBytesHWM = bytesPending;
   }
 
-  /*
-  ENC624J600 receive FIFO entry layout (in ascending address order):
+  /* Copy the packet header (including ENC624J600 data) into memory */
+  enc624j600_read_rxbuf(&theGlobals->chip, (unsigned char *) &theGlobals->rha, 
+    sizeof(theGlobals->rha) - sizeof(theGlobals->rha.workspace));
 
-    2 bytes         pointer to next entry (relative to chip base address)
-    6 bytes         receive status vector
-    6 bytes         destination address
-    6 bytes         source address
-    2 bytes         ethertype
-    ...             layer 3 payload
-  */
-  /* Read the above fields into the Receive Header Area */
-  enc624j600_read_rxbuf(&theGlobals->chip, theGlobals->recvHeaderArea, headerLen);
-
-  /* Next-packet pointer is little-endian and relative to the chip address
-  space. Convert this to a 'real' pointer. */
-  theGlobals->nextPkt = enc624j600_addr_to_ptr(
-      &theGlobals->chip, SWAPBYTES(*(unsigned short *)rhaPtr));
-  rhaPtr += sizeof(unsigned short);
-
-  /* Read packet length */
-  rsv = (enc624j600_rsv *)rhaPtr;
-  rhaPtr += sizeof(enc624j600_rsv);
-  pktLen = SWAPBYTES(rsv->pkt_len_le);
-
-  /* Read destination MAC */
-  destMac = rhaPtr;
-  rhaPtr += 6;
-
-  /* Skip over source MAC */
-  rhaPtr += 6;
-
-  /* Read protocol number/802.2 Type 1 length */
-  protocol = *(unsigned short *)rhaPtr;
-  rhaPtr += sizeof(unsigned short);
+  /* Packet length field in Recieve Status Vector is stored little-endian */
+  pktLen = SWAPBYTES(theGlobals->rha.rsv.pkt_len_le);
 
   /* Check for CRC errors. By default the ENC624J600 drops bad-CRC packets
   silently in hardware, but collect stats in case we disable that filter. */
-  if (RSV_BIT(*rsv, RSV_BIT_CRC_ERR)) {
+  if (RSV_BIT(theGlobals->rha.rsv, RSV_BIT_CRC_ERR)) {
     theGlobals->info.fcsErrors++;
     goto drop;
   }
@@ -156,15 +118,16 @@ static void handlePacket(driverGlobalsPtr theGlobals) {
   }
 
   /* Sanity-check our receive filters */
-  if (RSV_BIT(*rsv, RSV_BIT_UNICAST)) {
+  if (RSV_BIT(theGlobals->rha.rsv, RSV_BIT_UNICAST)) {
     /* Destination is broadcast or unicast to us */
     goto accept;
-  } else if (RSV_BIT(*rsv, RSV_BIT_BROADCAST)) {
+  } else if (RSV_BIT(theGlobals->rha.rsv, RSV_BIT_BROADCAST)) {
     theGlobals->info.broadcastRxFrameCount++;
     goto accept;
-  } else if (RSV_BIT(*rsv, RSV_BIT_MULTICAST) && RSV_BIT(*rsv, RSV_BIT_HASH_MATCH)) {
+  } else if (RSV_BIT(theGlobals->rha.rsv, RSV_BIT_MULTICAST) 
+             && RSV_BIT(theGlobals->rha.rsv, RSV_BIT_HASH_MATCH)) {
     /* Destination hash matches a multicast we're listening to */
-    if (findMulticastEntry(theGlobals, destMac)) {
+    if (findMulticastEntry(theGlobals, theGlobals->rha.dest)) {
       /* Actual destination address matches a multicast we're listening to */
       theGlobals->info.multicastRxFrameCount++;
       goto accept;
@@ -183,10 +146,10 @@ accept:
   /* An ethertype field of < 0x600 indicates an 802.2 Type 1 frame (Ethernet
   Phase II in Apple parlance). We assign this the protocol number 0. The LAP
   manager always registers itself as the handler for this protocol. */
-  if (protocol < 0x0600) {
+  if (theGlobals->rha.protocol < 0x0600) {
     protocolSlot = findPH(theGlobals, phProtocolPhaseII);
   } else {
-    protocolSlot = findPH(theGlobals, protocol);
+    protocolSlot = findPH(theGlobals, theGlobals->rha.protocol);
   }
   /* Search the protocol-handler table for a handler for this ethertype */
   if (protocolSlot == nil) {
@@ -194,15 +157,21 @@ accept:
     goto drop;
   }
 
-  /* Call the protocol handler to read the rest of the packet */
-  callPH(&theGlobals->chip, protocolSlot->handler, rhaPtr, ReadPacket,
+  /*
+  Call the protocol handler to read the rest of the packet.
+
+  pktLen-18 is length of packet minus header (6+6+2 bytes) and trailing checksum
+  (4 bytes)
+  */
+  callPH(&theGlobals->chip, protocolSlot->handler, theGlobals->rha.workspace,
          pktLen-18);
   theGlobals->info.rxFrameCount++;
 
 drop:
   /* finished with packet, discard any remaining data by advancing read pointer
-  and rx buffer tail to the start of the next packet */
-  enc624j600_update_rxptr(&theGlobals->chip, theGlobals->nextPkt);
+  and rx buffer tail to the start of the next packet  */
+  enc624j600_update_rxptr(&theGlobals->chip, enc624j600_addr_to_ptr(
+      &theGlobals->chip, SWAPBYTES(theGlobals->rha.nextPkt_le)));
 
   /* decrement pending-receive counter */
   enc624j600_decrement_rx_pending_count(&theGlobals->chip);

software/driver/multicast.c
@@ -21,7 +21,8 @@ multicastEntry* findMulticastEntry(const driverGlobalsPtr theGlobals,
 
 /* Find a free entry in the multicast table. Returns a pointer to the first free
 entry found, nil if no free entries */
-static multicastEntry* findFreeMulticastEntry(driverGlobalsPtr theGlobals) {
+static multicastEntry* findFreeMulticastEntry(
+    const driverGlobalsPtr theGlobals) {
   for (unsigned short i = 0; i < numberofMulticasts; i++) {
     if (theGlobals->multicasts[i].refCount == 0) {
       return &theGlobals->multicasts[i];

software/driver/multicast.h
@@ -4,6 +4,7 @@
 
 #include "driver.h"
 
-multicastEntry * findMulticastEntry(const driverGlobalsPtr theGlobals, const Byte address[6]);
+multicastEntry* findMulticastEntry(const driverGlobalsPtr theGlobals,
+                                   const Byte address[6]);
 OSStatus doEAddMulti(driverGlobalsPtr theGlobals, const EParamBlkPtr pb);
 OSStatus doEDelMulti(driverGlobalsPtr theGlobals, const EParamBlkPtr pb);

software/driver/protocolhandler.h
@@ -4,7 +4,8 @@
 
 #include "driver.h"
 
-protocolHandlerEntry * findPH(driverGlobalsPtr theGlobals, unsigned short theProtocol);
+protocolHandlerEntry* findPH(const driverGlobalsPtr theGlobals,
+                             const unsigned short theProtocol);
 OSStatus doEAttachPH(driverGlobalsPtr theGlobals, const EParamBlkPtr pb);
 OSStatus doEDetachPH(driverGlobalsPtr theGlobals, const EParamBlkPtr pb);
 void InitPHTable(driverGlobalsPtr theGlobals);

software/driver/registertools.c
@@ -7,7 +7,7 @@
 #include <MacErrors.h>
 #include <MacTypes.h>
 
-OSErr doENCReadReg(driverGlobalsPtr theGlobals, CntrlParamPtr pb) {
+OSErr doENCReadReg(const driverGlobalsPtr theGlobals, CntrlParamPtr pb) {
     ENCRegister * r = (ENCRegister *) pb->csParam;
     r->value = ENC624J600_READ_REG(&theGlobals->chip, r->reg);
     r->value = SWAPBYTES(r->value);
@@ -15,14 +15,14 @@ OSErr doENCReadReg(driverGlobalsPtr theGlobals, CntrlParamPtr pb) {
     return noErr;
 }
 
-OSErr doENCWriteReg(driverGlobalsPtr theGlobals, CntrlParamPtr pb) {
+OSErr doENCWriteReg(const driverGlobalsPtr theGlobals, const CntrlParamPtr pb) {
     ENCRegister * r = (ENCRegister *) pb->csParam;
     ENC624J600_WRITE_REG(&theGlobals->chip, r->reg, SWAPBYTES(r->value));
 
     return noErr;
 }
 
-OSErr doENCReadPhy(driverGlobalsPtr theGlobals, CntrlParamPtr pb) {
+OSErr doENCReadPhy(const driverGlobalsPtr theGlobals, CntrlParamPtr pb) {
     ENCRegister * r = (ENCRegister *) pb->csParam;
     r->value = enc624j600_read_phy_reg(&theGlobals->chip, r->reg);
     r->value = SWAPBYTES(r->value);
@@ -30,7 +30,7 @@ OSErr doENCReadPhy(driverGlobalsPtr theGlobals, CntrlParamPtr pb) {
     return noErr;
 }
 
-OSErr doENCWritePhy(driverGlobalsPtr theGlobals, CntrlParamPtr pb) {
+OSErr doENCWritePhy(const driverGlobalsPtr theGlobals, const CntrlParamPtr pb) {
     ENCRegister * r = (ENCRegister *) pb->csParam;
     ENC624J600_WRITE_REG(&theGlobals->chip, r->reg, SWAPBYTES(r->value));
     enc624j600_write_phy_reg(&theGlobals->chip, r->reg, SWAPBYTES(r->value));
@@ -38,12 +38,12 @@ OSErr doENCWritePhy(driverGlobalsPtr theGlobals, CntrlParamPtr pb) {
     return noErr;
 }
 
-OSErr doENCEnableLoopback(driverGlobalsPtr theGlobals) {
+OSErr doENCEnableLoopback(const driverGlobalsPtr theGlobals) {
     enc624j600_enable_phy_loopback(&theGlobals->chip);
     return noErr;
 }
 
-OSErr doENCDisableLoopback(driverGlobalsPtr theGlobals) {
+OSErr doENCDisableLoopback(const driverGlobalsPtr theGlobals) {
     enc624j600_disable_phy_loopback(&theGlobals->chip);
     return noErr;
 }

commit e2eb98df36e5472da2a8c2b39886474fd30062aa

Fix broken FindPH
The changes in eec45d1e9658bfd78ce5c77dff64bb0697503e72 caused FindPH to
throw an error when trying to find an empty PH table slot.

software/driver/protocolhandler.c
@@ -17,8 +17,7 @@ Returns a pointer to the entry. */
 protocolHandlerEntry *findPH(const driverGlobalsPtr theGlobals,
                              const unsigned short theProtocol) {
   for (unsigned short i = 0; i < numberOfPhs; i++) {
-    if (theGlobals->protocolHandlers[i].ethertype == theProtocol &&
-        theGlobals->protocolHandlers[i].handler != nil) {
+    if (theGlobals->protocolHandlers[i].ethertype == theProtocol) {
       return &theGlobals->protocolHandlers[i];
     }
   }