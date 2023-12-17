982313f25a005ec3a28f5000be92e05d0f00f8ae
Richard Halkyard <rhalkyard@gmail.com>
2023-12-17 13:50:26 -0600

    Use system-provided icons
    Turns out that the system already gives 'comd' files the appropriate
    icon if their creator code is 'Enet'. Do this instead of trying and
    failing to bring our own icon.
    
    Fixes issue #9

--- software/driver/SEthernet30.r
+++ software/driver/SEthernet30.r
@@ -6,28 +6,10 @@ data 'enet' (9635, locked, sysheap, "SEthernet/30") {
     $$read("SE30_driver.resource")
 };
 
-resource 'vers' (1) {
+resource 'vers' (1, purgeable) {
     0x00, 0x00;
     development, 0x00;
     0;
     "0.0.0";
     "0.0.0";
 };
-
-resource 'FREF' (128) {
-    'comd',
-    0,
-    ""
-};
-
-resource 'BNDL' (128) {
-    'sEth',
-    0,
-    {
-        'FREF', {0, 128},
-        'ICN#', {0, 128}
-    }
-};
-
-type 'sEth' as 'STR ';
-resource 'sEth' (0, purgeable) { "SEthernet/30 Driver" };
